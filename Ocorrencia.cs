43fc9b3af9438ab5d953a7e824ef6b9b94604a57
luisc13 <luisinho0903@gmail.com>
2020-03-26 19:04:09 -0300

    Tela de ocorrência adicionada

---
+++ Ocorrencia.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace EasyCall
+{
+    public partial class Ocorrencia : Form
+    {
+        public Ocorrencia()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            String Ocor;
+            Ocor =Convert.ToString(TxbOcorrencia);
+            MessageBox.Show("Enviado com Sucesso");
+            this.Close();
+
+        }
+    }
+}
