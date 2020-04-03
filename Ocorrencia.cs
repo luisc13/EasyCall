e76f1dbb983142b039346e8c9c33f15734dcaaea
luisc13 <luisinho0903@gmail.com>
2020-04-03 20:11:53 -0300

    Metodo concluido

--- Ocorrencia.cs
+++ Ocorrencia.cs
@@ -4,6 +4,7 @@ using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -27,7 +28,23 @@ namespace EasyCall
             var email = new Email();
             MessageBox.Show(email.enviarEmail(ocr).ToString());
 
+            
+            SqlCommand cmd = new SqlCommand();
+            Conexao conexao = new Conexao();
+            SqlDataReader dr;
+
+            cmd.CommandText = "INSERT INTO DIVIDA(Ocorrencia) VALUES (@ocorrencia)";
+            cmd.Parameters.AddWithValue("@ocorrencia", ocr);
             this.Close();
+            try
+            {
+                cmd.Connection = conexao.conectar();
+                dr = cmd.ExecuteReader();
+            }
+            catch (SqlException ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
 
         }
     }
