6f1345d782f261cb12d62ba0db4a59a5333d88d6
Gustavo Bonfim <gunais1234@gmail.com>
2020-04-16 14:00:18 -0300

    restore conexao.cs

---
+++ DAO/Conexao.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace EasyCall.DAO
+{
+    class Conexao
+    {
+        SqlConnection con = new SqlConnection();
+
+        public Conexao()
+        {
+            con.ConnectionString = @"Data Source=LAPTOP-OGHHCABF\SQLEXPRESS;Initial Catalog=EasyCall;User ID=sa;Password=toledo";
+        }
+
+        public SqlConnection conectar ()
+        {
+            try
+            {
+                if (con.State == System.Data.ConnectionState.Closed)
+                {
+                    con.Open();
+                }
+            }
+            catch (SqlException ex)
+            {
+                MessageBox.Show("Unexpected error:" + ex.Message);
+            }
+            return con;
+        }
+
+        public void desconectar ()
+        {
+            if (con.State == System.Data.ConnectionState.Open)
+            {
+                con.Close();
+            }
+        }
+    }
+}
