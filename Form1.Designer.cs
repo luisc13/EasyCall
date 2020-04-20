9367f04a3716387ad0591c52f58aed67dbd576e1
Gustavo Bonfim <gunais1234@gmail.com>
2020-04-20 19:59:19 -0300

    bug fix on form1 Design

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -158,7 +158,6 @@
             this.Name = "Form1";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Form1";
-            this.Load += new System.EventHandler(this.Form1_Load);
             this.panel1.ResumeLayout(false);
             this.panel1.PerformLayout();
             this.ResumeLayout(false);
