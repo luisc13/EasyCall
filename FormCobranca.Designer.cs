062fbf4ba91ca46f3ed95354653bdb1e9590890c
luisc13 <luisinho0903@gmail.com>
2020-05-23 15:52:07 -0300

    Tela cobrança att2.0

--- FormCobranca.Designer.cs
+++ FormCobranca.Designer.cs
@@ -126,7 +126,7 @@
             // 
             // btnDesligar
             // 
-            this.btnDesligar.Location = new System.Drawing.Point(502, 422);
+            this.btnDesligar.Location = new System.Drawing.Point(394, 422);
             this.btnDesligar.Name = "btnDesligar";
             this.btnDesligar.Size = new System.Drawing.Size(102, 45);
             this.btnDesligar.TabIndex = 16;
@@ -281,7 +281,7 @@
             // 
             // btnEmail
             // 
-            this.btnEmail.Location = new System.Drawing.Point(394, 422);
+            this.btnEmail.Location = new System.Drawing.Point(511, 422);
             this.btnEmail.Name = "btnEmail";
             this.btnEmail.Size = new System.Drawing.Size(102, 45);
             this.btnEmail.TabIndex = 28;
@@ -295,7 +295,7 @@
             this.relatóriosToolStripMenuItem});
             this.menuStrip1.Location = new System.Drawing.Point(0, 0);
             this.menuStrip1.Name = "menuStrip1";
-            this.menuStrip1.Size = new System.Drawing.Size(1030, 24);
+            this.menuStrip1.Size = new System.Drawing.Size(1103, 24);
             this.menuStrip1.TabIndex = 29;
             this.menuStrip1.Text = "menuStrip1";
             // 
@@ -426,7 +426,7 @@
             this.panel4.Controls.Add(this.lbxDevedor);
             this.panel4.Location = new System.Drawing.Point(12, 27);
             this.panel4.Name = "panel4";
-            this.panel4.Size = new System.Drawing.Size(628, 122);
+            this.panel4.Size = new System.Drawing.Size(592, 132);
             this.panel4.TabIndex = 38;
             // 
             // label15
@@ -442,9 +442,9 @@
             // lbxDevedor
             // 
             this.lbxDevedor.HideSelection = false;
-            this.lbxDevedor.Location = new System.Drawing.Point(9, 22);
+            this.lbxDevedor.Location = new System.Drawing.Point(19, 30);
             this.lbxDevedor.Name = "lbxDevedor";
-            this.lbxDevedor.Size = new System.Drawing.Size(600, 97);
+            this.lbxDevedor.Size = new System.Drawing.Size(561, 97);
             this.lbxDevedor.TabIndex = 0;
             this.lbxDevedor.UseCompatibleStateImageBehavior = false;
             // 
@@ -453,7 +453,7 @@
             this.panel5.BackColor = System.Drawing.Color.PeachPuff;
             this.panel5.Controls.Add(this.label14);
             this.panel5.Controls.Add(this.lbxOcorrencia);
-            this.panel5.Location = new System.Drawing.Point(662, 60);
+            this.panel5.Location = new System.Drawing.Point(610, 27);
             this.panel5.Name = "panel5";
             this.panel5.Size = new System.Drawing.Size(328, 352);
             this.panel5.TabIndex = 41;
@@ -481,7 +481,7 @@
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(188)))), ((int)(((byte)(210)))), ((int)(((byte)(238)))));
-            this.ClientSize = new System.Drawing.Size(1030, 485);
+            this.ClientSize = new System.Drawing.Size(1103, 471);
             this.Controls.Add(this.panel5);
             this.Controls.Add(this.panel4);
             this.Controls.Add(this.button1);
